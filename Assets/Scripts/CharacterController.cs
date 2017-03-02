7ce289bf0e9264423f8e528e5f6f95f2bb9d40cb
Daniel Jones <d.mullerjones@gmail.com>
2017-03-02 12:44:01 -0300

    Adiçao de Inimigo, Primeira cena

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+	public float speed;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+	
+		rb = GetComponent<Rigidbody> ();
+		speed = 20;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(new Vector3(-1,0,0) * speed * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(new Vector3(1,0,0) * speed * Time.deltaTime);
+		}
+
+	}
+}
