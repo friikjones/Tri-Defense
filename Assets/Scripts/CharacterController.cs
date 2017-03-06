83ea03e78a98f6aaf5d84456feaf836881434ff7
Daniel Jones <d.mullerjones@gmail.com>
2017-03-06 12:21:17 -0300

    Controle basico do personagem, Ideas.txt

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,26 +5,47 @@ public class CharacterController : MonoBehaviour {
 
 	public float speed;
 	public Rigidbody rb;
+	public int controllerType;
 
 	// Use this for initialization
 	void Start () {
 	
 		rb = GetComponent<Rigidbody> ();
 		speed = 20;
+		controllerType = 1; //Keyboard
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-		if(Input.GetKey(KeyCode.A))
+		if (controllerType == 1)
+		Char_Move(KeyCode.W,KeyCode.S,KeyCode.A,KeyCode.D);
+	}
+	
+	void Char_Move (KeyCode Up, KeyCode Down, KeyCode Left, KeyCode Right)
+	{
+		
+		if(Input.GetKey(Up))
 		{
-			transform.Translate(new Vector3(-1,0,0) * speed * Time.deltaTime);
+			transform.Translate(new Vector3(0,0,1) * speed * Time.deltaTime, Space.World);
 		}
-		if(Input.GetKey(KeyCode.D))
+		if(Input.GetKey(Down))
 		{
-			transform.Translate(new Vector3(1,0,0) * speed * Time.deltaTime);
+			transform.Translate(new Vector3(0,0,-1) * speed * Time.deltaTime, Space.World);
 		}
-
+		if(Input.GetKey(Left))
+		{
+			transform.Translate(new Vector3(-1,0,0) * speed * Time.deltaTime, Space.World);
+		}
+		if(Input.GetKey(Right))
+		{
+			transform.Translate(new Vector3(1,0,0) * speed * Time.deltaTime, Space.World);
+		}
+		
+		transform.LookAt(new Vector3(0,0,0));		//Vira o personagem para a Arvore
+		transform.Rotate(new Vector3(0,180,0));		//Vira de costas
+		
+		
 	}
 }
